ba79880f36a8eada70c9329718a7ccf2341ad2f9
John Isaiah Sangalang <esang037@gmail.com>
2025-11-04 22:43:55 +1300

    add markdown to blocks

--- src/utils.py
+++ src/utils.py
@@ -120,3 +120,28 @@ def text_to_textnodes(text):
     # Split for links
     nodes = split_nodes_link(nodes)
     return nodes
+
+# takes a raw markdown string and returns a list of "blocks" strings
+def markdown_to_blocks(markdown):
+    blocks = []
+    current_block = []
+    lines = markdown.split("\n")
+    
+    # loop through the lines
+    # we also want to remove any "empty" blocks due to excessive lines
+    for line in lines:
+        # we want to strip any leading or trailing whitespace
+        stripped_line = line.strip()
+        if stripped_line == "":
+            # if we hit an empty line, we finalize the current block
+            if current_block:
+                blocks.append("\n".join(current_block).strip())
+                current_block = []
+        else:
+            current_block.append(stripped_line)
+
+    # add the last block if exists
+    if current_block:
+        blocks.append("\n".join(current_block).strip())
+
+    return blocks
